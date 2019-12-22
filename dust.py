a1e32d47e0fb2856e7d28b7d4f1374ecd545acc6
dddfff22 <32073145+dddfff22@users.noreply.github.com>
2019-12-22 15:19:17 +0900

    Update dust.py

--- dust.py
+++ dust.py
@@ -6,29 +6,58 @@ import time
 ser=serial.Serial("dev/ttyACM0",9600)
 username="dddfff22"
 while True:
-    res= ser.readline()
-    print(res.decode())
-    data = {
-      "user_id": username,
-      "rooms": [
-        
-      ],
-      "timestamp": "2019-12-21T13:33:36.328Z"
-    }
-    data["rooms"].append({
-          "dust": "string",
-          "humidity": "string",
-          "roomId": "string",
-          "roomName": "string",
-          "temp": "string"
+    try:
+      res= ser.readline()
+      print(res.decode())
+      temp=res.decode.split(" ")
+      if len(temp)>2:
+        hu = temp[0]
+        tu = temp[3].split("C")
+      else
+        du=temp[0].split("\n")[0]
+      res= ser.readline()
+      print(res.decode())
+      temp=res.decode.split(" ")
+      if len(temp)>2:
+        hu = temp[0]
+        tu = temp[3].split("C")
+      else
+        du=temp[0].split("\n")[0]
+      
+
+      data = {
+        "user_id": username,
+        "rooms": [
+          {
+            "dust": str(du),
+            "humidity": str(hu),
+            "roomId": "dd",
+            "roomName": "Living Room",
+            "temp": str(tu)
+        }
+        ],
+        "timestamp": "2019-12-21T13:33:36.328Z"
+      }
+
+      data["rooms"].append({
+            "dust": "100",
+            "humidity": "60",
+            "roomId": "string",
+            "roomName": "",
+            "temp": "35"
       })
-    data = json.dumps(data)
-    data = str(data)
-    data = data.encode('utf-8')
-    req = request.Request('http://45.119.146.82:8082/dust/addDustInfo/', data=data)
-    req.add_header('Content-Type', 'application/json')
-    response = request.urlopen(req)
-    print (response.read())
-    time(100000)
+      
+      data = json.dumps(data)
+      data = str(data)
+      data = data.encode('utf-8')
+      req = request.Request('http://45.119.146.82:8082/dust/addDustInfo/', data=data)
+      req.add_header('Content-Type', 'application/json')
+      response = request.urlopen(req)
+      print (response.read())
+      time.sleep(5)
+      ser.flushInput()
+      ser.flushOutput()
+    except:
+      print("error")
 
 ser.close()
